0c9a30766f70a3abb21a74b927c1b465e75b124a
Giovani SFPV <giovanisfpv@gmail.com>
2024-07-25 13:42:41 -0300

    Init

--- Two Sum/Program.cs
+++ Two Sum/Program.cs
@@ -1,15 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
-Console.WriteLine("Hello, World!");
-Console.WriteLine("tT");
+﻿class Tests
+{
+  public int[] samples;
+  public int target;
+}
 
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Clear();
+    Tests test1 = new Tests();
+    test1.samples = new int[] { 0, 4, 3, 0 };
+    test1.target = 0;
 
+    var result = TwoSum(test1.samples, test1.target);
+    Console.WriteLine(result[1] + ", " + result[0]);
+  }
 
-// Dictionary<int[], int> test1 = new Dictionary<int[] , int>;
+  static public int[] TwoSum(int[] nums, int target)
+  {
+    var sortedNums = new int[nums.Length];
+    Array.Copy(nums, sortedNums, nums.Length);
+    Array.Sort(sortedNums);
+    // var sortedNums = nums.Where(x => x < target).ToArray();
+    for (int i = 0; i < sortedNums.Length; i++)
+    {
+      for (int j = (i + 1); j < sortedNums.Length; j++)
+      {
+        if ((sortedNums[i] + sortedNums[j]) == target)
+        {
+          int firstIndex = Array.IndexOf(nums, sortedNums[j]);
+          if (sortedNums[i] == sortedNums[j])
+          {
+            nums[firstIndex] = sortedNums[i] + 1;
+          }
+          Console.WriteLine(sortedNums[i] + " " + nums[firstIndex]);
+          int secondIndex = Array.IndexOf(nums, sortedNums[i]);
+          Console.WriteLine(sortedNums[j]);
+          return [firstIndex, secondIndex];
+        }
+      }
 
-// public int[] twoSum(int[], int)
-// {
-//
-// }
+    }
+    return [0, 0];
 
-// twoSum()
+  }
+}
