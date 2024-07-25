d51365131070cb8a135cf9cc991bfeb801f9b88c
Giovani SFPV <giovanisfpv@gmail.com>
2024-07-25 10:58:57 -0300

    Init

---
+++ Two Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Hello, World!");
+Console.WriteLine("tT");
+
+
+
+// Dictionary<int[], int> test1 = new Dictionary<int[] , int>;
+
+// public int[] twoSum(int[], int)
+// {
+//
+// }
+
+// twoSum()
